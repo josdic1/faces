4e0b98572a9977491fc08c2260d339c83772693b
josdic1 <joshuadicker@gmail.com>
2025-02-06 14:54:16 -0500

    added a second list for the cart, and captured all inputs into a controlled object for entering cc info

--- fake-pay-sys/src/index.js
+++ fake-pay-sys/src/index.js
@@ -12,9 +12,15 @@ const init = () => {
   let inEditMode = false
   let items = []
   let cartItems = []
-  let formData = {
+  let itemFormData = {
     name: '',
     price: 0
+  }
+  let ccFormData = {
+    name: '',
+    ccNumber: '',
+    exp: '',
+    cvv: ''
   }
   let selectedItem = {
     id: '',
@@ -25,7 +31,8 @@ const init = () => {
   // initial fetch
   fetchItems()
 
-  // render functions
+  // LIST
+  //<---- render list ---->
   function renderList(data) {
 
     const itemsList = data.map(i => (
@@ -34,11 +41,11 @@ const init = () => {
         <td>${i.name}</td>
         <td>${i.price}</td>
         <td>
-          <button type="button" class="list-button" id="${i.id}" name="add">Add 🛒</button>
+          <button type="button" class="list-button add" id="${i.id}" name="add">Add 🛒</button>
         </td>
       </tr>`
     ))
-
+    cart
     const listHtml =
       `<table>
       <thead>
@@ -50,12 +57,89 @@ const init = () => {
         </tr>
       </thead>
       <tbody>
-        ${itemsList}
+        ${itemsList.join('')}
       </tbody>
     </table>`
 
     list.innerHTML = listHtml
+
+    list.querySelectorAll('.list-button.add').forEach(btn => (
+      btn.addEventListener('click', handleAddToCartClick)
+    ))
   }
+
+
+  //<---- list handler functions ---->
+
+  function handleAddToCartClick(e) {
+    const { id, name } = e.target
+    console.log(id, name)
+  }
+
+  // CART
+  //<---- render cart ---->
+  function renderCart(cItems) {
+    menu.textContent = cItems.length
+
+    const cartItemsList = cItems.map(cItem => (
+      `<tr id="${cItem.id}">
+        <td>${cItem.id}</td>
+        <td>${cItem.name}</td>
+        <td>${cItem.price}</td>
+         <td>#</td>
+        <td>
+          <button type="button" class="list-button remove" id="${cItem.id}" name="remove">Remove</button>
+        </td>
+      </tr>`
+    ))
+
+    const cartHtml =
+      `
+      <h1>My Cart (${cartItems.length + " items"})</h1>
+      <table id="myCart">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>NAME</th>
+            <th>$</th>
+            <th>Quantity</th>
+            <th>𝕏</th>
+            
+          </tr>
+        </thead>
+        <tbody>
+          ${cartItemsList.join('')}
+        </tbody>
+      </table>`
+
+    cart.innerHTML = cartHtml
+  }
+
+  function renderPayment() {
+    const paymentHtml =
+      `<form id="form">
+        <input type="text" class="form-input" id="name" placeholder="Cardholder name..." />
+         <input type="text" class="form-input" id="ccNumber" placeholder="Card #..." />
+        <input type="date" class="form-input" id="exp"  />
+          <input type="text" class="form-input" id="cvv"  /><br>
+          <button type="submit" class="form-input button" id="submit">🛒 Checkout</button>
+          <button type="button" class="form-input button" id="cancel">Cancel</button>
+      </form>`
+
+    payment.innerHTML = paymentHtml
+
+    payment.querySelectorAll('.form-input').forEach(input => {
+      input.addEventListener("input", handleFormInput)
+    })
+
+  }
+
+  // payment handler functions
+  function handleFormInpute) {
+
+  }
+
+
 
 
   // async CRUD
@@ -68,8 +152,23 @@ const init = () => {
       const data = await r.json()
       items = data
       renderList(data)
+      fetchCart()
+      renderPayment()
     } catch (error) { console.error(error) }
   }
+
+  async function fetchCart() {
+    try {
+      const r = await fetch(`http://localhost:3000/cartItems`)
+      if (!r.ok) {
+        throw new Error('GET: bad fetch')
+      }
+      const data = await r.json()
+      cartItems = data
+      renderCart(data)
+    } catch (error) { console.error(error) }
+  }
+
 
 }
 
